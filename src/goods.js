07204ec57cb63facaef7ada572ecd336b85974d7
Andrew Tebenko <andrewtebenko@gmail.com>
2023-01-09 21:57:47 +0200

    Add an array of data on the product

---
+++ src/goods.js
@@ -0,0 +1,52 @@
+const goods = [
+    {
+        id: 0,
+        src: './img/goods/coffee-1.png',
+        name: 'Solimo Coffee Beans 2 kg',
+        country: '',
+        price: '10.73$',
+        recommended: true
+    },
+    {
+        id: 1,
+        src: './img/goods/coffee-2.png',
+        name: 'Presto Coffee Beans 1 kg',
+        country: '',
+        price: '15.99$',
+        recommended: true
+    },
+    {
+        id: 2,
+        src: './img/goods/coffee-3.png',
+        name: 'AROMISTICO Coffee 1 kg',
+        country: '',
+        price: '6.99$',
+        recommended: true
+    },
+    {
+        id: 3,
+        src: './img/goods/coffee-3.png',
+        name: 'AROMISTICO Coffee 1 kg',
+        country: 'Brazil',
+        price: '6.99$',
+        recommended: false
+    },
+    {
+        id: 4,
+        src: './img/goods/coffee-3.png',
+        name: 'AROMISTICO Coffee 1 kg',
+        country: 'Kenya',
+        price: '6.99$',
+        recommended: false
+    },
+    {
+        id: 5,
+        src: './img/goods/coffee-3.png',
+        name: 'AROMISTICO Coffee 1 kg',
+        country: 'Columbia',
+        price: '6.99$',
+        recommended: false
+    }
+]
+
+export {goods};
