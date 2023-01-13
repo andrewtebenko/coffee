c7116342af41de31463b440c7baffc71989634ba
Andrew Tebenko <andrewtebenko@gmail.com>
2023-01-13 20:49:36 +0200

    Add data to the data file

--- src/goods.js
+++ src/goods.js
@@ -25,15 +25,15 @@ const goods = [
     },
     {
         id: 3,
-        src: './img/goods/coffee-3.png',
-        name: 'AROMISTICO Coffee 1 kg',
+        src: './img/goods/coffee-4.png',
+        name: 'TANZANIA Iyenga 1 kg',
         country: 'Brazil',
-        price: '6.99$',
+        price: '7.50$',
         recommended: false
     },
     {
         id: 4,
-        src: './img/goods/coffee-3.png',
+        src: './img/goods/coffee-5.png',
         name: 'AROMISTICO Coffee 1 kg',
         country: 'Kenya',
         price: '6.99$',
@@ -41,10 +41,34 @@ const goods = [
     },
     {
         id: 5,
-        src: './img/goods/coffee-3.png',
-        name: 'AROMISTICO Coffee 1 kg',
+        src: './img/goods/coffee-6.png',
+        name: 'ROASTERS Coffee 1 kg',
         country: 'Columbia',
         price: '6.99$',
+        recommended: false
+    },
+    {
+        id: 6,
+        src: './img/goods/coffee-7.png',
+        name: 'HONDURAS HG EP 1 kg',
+        country: 'Brazil',
+        price: '10.00$',
+        recommended: false
+    },
+    {
+        id: 7,
+        src: './img/goods/coffee-8.png',
+        name: 'BEANGO Coffee 1 kg',
+        country: 'Columbia',
+        price: '6.80$',
+        recommended: false
+    },
+    {
+        id: 8,
+        src: './img/goods/coffee-9.png',
+        name: 'DRIPS TanzaniaCoffee 1 kg',
+        country: 'Kenya',
+        price: '7.99$',
         recommended: false
     }
 ]
