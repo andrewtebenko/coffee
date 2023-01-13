38ddbed9a6b204dbc13ab3d52e6be59694a7fc5a
Andrew Tebenko <andrewtebenko@gmail.com>
2023-01-13 20:54:00 +0200

    Add a component describing goods to the coffee page

---
+++ src/components/product-description/product-description.js
@@ -0,0 +1,29 @@
+import Beans from '../beans/beans';
+
+import './product-description.scss';
+
+const ProductDescription = ({src, title, country, isOpen, description, price}) => {
+    return (
+        <div className="product">
+            <div className="container">
+                <div className="row">
+                    <div className="offset-1 col-10 offset-1">
+                        <div className="product_wrapper">
+                            <img src={src} alt="coffee" className="product-img"/>
+                            <div className="product-description_block">
+                                <h2 className="title-description">{title}</h2>
+                                <Beans color={{color: "#000000"}} 
+                                       img={"./img/component-images/coffee-beans-black.png"} />
+                                <p className="product_country">Country:<span> {country}</span></p>
+                                <p className="product_description">Description:<span> {description}</span></p>
+                                <p className="product_price">Price:<span> {price}</span></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ProductDescription;
--- src/pages/coffee/coffee.js
+++ src/pages/coffee/coffee.js
@@ -4,6 +4,7 @@ import Proposal from '../../components/proposal/proposal';
 import CardFilter from '../../components/card-filter/card-filter';
 import CardList from '../../components/card-list/card-list';
 import Footer from '../../components/footer/footer';
+import ProductDescription from '../../components/product-description/product-description';
 
 import './coffee.scss';
 
@@ -11,21 +12,58 @@ const p1 = `Extremity sweetness difficult behaviour he of. On disposal of as lan
 const p2 = `Afraid at highly months do things on at. Situation recommend objection do intention
       so questions. As greatly removed calling pleased improve an. Last ask him cold feel
       met spot shy want. Children me laughing we prospect answered followed. At it went
-      is song that held help face.`
+      is song that held help face.`;
+
+const title = `About it`;
+
+const description = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim 
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.`;
+
+const src = 'img/component-images/description-photo1.jpg';
 
 export class Coffee extends Component {
+    
+    constructor(props) {
+        super(props);
+        this.state = {
+            isOpen: false
+        }
+    }
+
+    onOpenProducts = (id) => {
+        const {data} = this.props
+        data.forEach(item => {
+            if (item.id === id) {
+                this.src= src;
+                this.country = item.country;
+                this.description = description;
+                this.price = item.price;
+            }
+        })
+        this.setState({
+            isOpen: true
+        })
+    }
+
     render() {
+        const {isOpen} = this.state;
         return (
             <>
                 <Header background={'/img/background-images/coffee-bg.png'} title={'Our Coffee'}/>
-                <Proposal src={'img/component-images/girl.jpg'} 
-                       alt={'girl'}
-                       title={'About our beans'}
-                       p1={p1} p2={p2} />
-                <CardFilter/>
-                <div className="offset-2 col-8 offset-2 coffee-wrapper">
-                    <CardList data={this.props.data} onOpenDescription={this.onOpenDescription}/>
-                </div>
+                {isOpen ?
+                    <ProductDescription title={title} isOpen={isOpen} src={this.src} country={this.country} 
+                        description={this.description} price={this.price}/>
+                    :
+                    <>
+                        <Proposal src={'img/component-images/girl.jpg'} alt={'girl'}
+                            title={'About our beans'} p1={p1} p2={p2} />
+                        <CardFilter/>
+                        <div className="offset-2 col-8 offset-2 coffee-wrapper">
+                            <CardList data={this.props.data} onOpenProducts={this.onOpenProducts}/>
+                        </div>
+                    </>    
+                }
                 <Footer />
             </>
         )
